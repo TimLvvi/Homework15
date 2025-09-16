4bf2bc723fc0cd8c62d3c3716cc1a708c98cf212
TimLvv <lavrentev.timur@mail.ru>
2025-09-16 17:17:27 +0300

    commit 4

--- src/main/java/org/skypro/skyshop/controller/ShopController.java
+++ src/main/java/org/skypro/skyshop/controller/ShopController.java
@@ -1,39 +1,56 @@
 package org.skypro.skyshop.controller;
 
 import org.skypro.skyshop.model.article.Article;
+import org.skypro.skyshop.model.basket.UserBasket;
 import org.skypro.skyshop.model.product.Product;
 import org.skypro.skyshop.model.search.SearchResult;
 import org.skypro.skyshop.model.search.SearchService;
+import org.skypro.skyshop.service.BasketService;
 import org.skypro.skyshop.service.StorageService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.UUID;
 
 @RestController
 public class ShopController {
 
     private final SearchService searchService;
     private final StorageService storageService;
+    private final BasketService basketService;
 
-    public ShopController(StorageService storageService, SearchService searchService) {
+    public ShopController(StorageService storageService, SearchService searchService, BasketService basketService) {
         this.storageService = storageService;
         this.searchService = searchService;
+        this.basketService = basketService;
     }
 
     @GetMapping("/products")
     public Collection<Product> getAllProducts() {
-        return storageService.getProductCollection() ;
+        return storageService.getProductCollection();
     }
 
     @GetMapping("/articles")
     public Collection<Article> getAllArticles() {
         return storageService.getArticleCollection();
     }
+
     @GetMapping("/search")
     public Collection<SearchResult> search(@RequestParam String pattern) {
         return searchService.search(pattern);
     }
 
+    @GetMapping("/basket/{id}")
+    public String addProduct(@PathVariable("id") UUID id) {
+        basketService.add(id);
+        return "продкут добавлен ";
+    }
+
+    @GetMapping("/basket")
+    public UserBasket getUserBasket() {
+        return basketService.getUserBasket();
+    }
 }
